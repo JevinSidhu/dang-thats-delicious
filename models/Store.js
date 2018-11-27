3e809856c13d4b6c26bade027b95bd9d8e76e791
Jevin Sidhu <jevin.sidhu@gmail.com>
2018-11-26 22:29:39 -0500

    init commit

---
+++ models/Store.js
@@ -0,0 +1,31 @@
+const mongoose = require('mongoose');
+mongoose.Promise = global.Promise;
+const slug = require('slugs');
+
+const storeSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    trim: true,
+    required: 'Please enter a store name!',
+  },
+  slug: String,
+  description: {
+    type: String,
+    trim: true,
+  },
+  tags: [String],
+});
+
+// before the storeSchema is saved, we are going to run a function
+// use a proper function to use _this_, the store!
+// run name through slug pkg we imported to create slug
+storeSchema.pre('save', function(next) {
+  if (!this.isModified('name')) {
+    next(); // skip it
+    return; // stop this function from running
+  }
+  this.slug = slug(this.name);
+  next(); // save!
+});
+
+module.exports = mongoose.model('Store', storeSchema);
